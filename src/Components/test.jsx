d7578cd2926843bc3c53508e16a4e710a5a6647b
Darien <darien.miles@gmail.com>
2019-11-15 17:38:04 +0900

    Clean up on menu coding and more prototypes

--- src/Components/test.jsx
+++ src/Components/test.jsx
@@ -1,43 +1,18 @@
 import React, { Component } from "react";
 let arr = ["Choose for me", "Let me choose", "Store", "Outfits"];
-// function testTest(x) {
-//   let menuArray = x.slice();
-//   menuArray.push(menuArray[0]);
-//   menuArray.shift(menuArray[0]);
-//   arr = menuArray;
-//   console.log(arr);
-// }
 class Test extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { menuArray: arr };
-  }
+  state = {
+    menuArray: arr
+  };
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.update(), 1000);
-  }
-
-  // componentWillUnmount() {
-  //   clearInterval();
-  // }
-
-  update() {
-    this.setState({
-      menuArray: arr
-    });
-  }
-  handleClick() {
+  handleMenu = () => {
     let menuArray = arr.slice();
     menuArray.push(menuArray[0]);
     menuArray.shift(menuArray[0]);
     arr = menuArray;
-  }
+    this.setState({ menuArray: arr });
+  };
 
-  // arrayPushShift = pushShift => {
-  //   console.log(pushShift);
-
-  //   this.setState({});
-  // };
   render() {
     return (
       <React.Fragment>
@@ -46,7 +21,7 @@ class Test extends Component {
           <h1 className="logo-main">ai closet</h1>
         </header>
         <div id="main">
-          <nav id="slide-nav" class="full-height" onClick={this.handleClick}>
+          <nav id="slide-nav" class="full-height" onClick={this.handleMenu}>
             <ul>
               {this.state.menuArray.map(menuArray => (
                 <li key={menuArray}>
