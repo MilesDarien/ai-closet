9c5cea2be0c2480370f7d989d171de51b47353e7
Darien <darien.miles@gmail.com>
2019-11-21 12:48:39 +0900

    weather api test

--- src/App.js
+++ src/App.js
@@ -1,75 +1,61 @@
 import React, { Component } from "react";
 import { Link, Route, Switch } from "react-router-dom";
-import { useSwipeable, Swipeable } from 'react-swipeable';
+import { useSwipeable, Swipeable } from "react-swipeable";
 
 let arr = ["Choose for me", "Let me choose", "Store", "Outfits"];
-let links = ["/AiCoordinate","/LetMeChoose", "/Store", "/Outfits"];
+let links = ["/AiCoordinate", "/LetMeChoose", "/Store", "/Outfits"];
 class Main extends Component {
   state = {
     menuArray: arr,
-    menuLinks: links,
+    menuLinks: links
   };
 
-//  handleLinks = () => {
-//    let spliceLinks = links.slice();
-//    spliceLinks.push(spliceLinks[0]);
-//   spliceLinks.shift(spliceLinks[0]);
-//   links = spliceLinks;
-//   this.setState({ menuLinks: links });
-//  }
+  //manipulates array of links and Names to move menu up
+  pushShift = (array, x) => {
+    array.push(array[x]);
+    array.shift(array[x]);
+  };
+
+  //takes the array of names and links and shifts everything down
+  popUnshift = array => {
+    let popItem = array.pop();
+    array.unshift(popItem);
+  };
+
   handleMenuUp = () => {
     let menuArray = arr.slice();
-    menuArray.push(menuArray[0]);
-    menuArray.shift(menuArray[0]);
     let spliceLinks = links.slice();
-   spliceLinks.push(spliceLinks[0]);
-  spliceLinks.shift(spliceLinks[0]);
-  links = spliceLinks;
+    this.pushShift(menuArray, 0);
+    this.pushShift(spliceLinks, 0);
+    links = spliceLinks;
     arr = menuArray;
     this.setState({ menuArray: arr });
     this.setState({ menuLinks: links });
-    
   };
-
-//   handleLinksDown = () => {
-//    let spliceLinks = links.slice();
-//    let linkTop = spliceLinks.pop();
-//   spliceLinks.unshift(linkTop);
-//   links = spliceLinks;
-//   this.setState({ menuLinks: links });
-//  }
 
   handleMenuDown = () => {
     let menuArray = arr.slice();
-    let menuTop = menuArray.pop();
-    menuArray.unshift(menuTop);
     let spliceLinks = links.slice();
-   let linkTop = spliceLinks.pop();
-  spliceLinks.unshift(linkTop);
-  links = spliceLinks;
+    this.popUnshift(menuArray);
+    this.popUnshift(spliceLinks);
+
+    links = spliceLinks;
     arr = menuArray;
     this.setState({ menuArray: arr });
     this.setState({ menuLinks: links });
   };
-    
-
 
   render() {
     let links = this.state.menuLinks;
     let names = this.state.menuArray;
-    
-  const config ={
-    onSwipedUp: () => this.handleMenuUp(),
-    onSwipedDown: ()=> this.handleMenuDown(),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true
-  };
 
+    const config = {
+      onSwipedUp: () => this.handleMenuUp(),
+      onSwipedDown: () => this.handleMenuDown(),
+      preventDefaultTouchmoveEvent: true,
+      trackMouse: true
+    };
 
-
-  // const handlers = useSwipeable({ onSwipedUp: (eventData) => eventHandler, ...config })
-
-    
     return (
       <React.Fragment>
         <header>
@@ -77,6 +63,11 @@ class Main extends Component {
           <h1 className="logo-main">ai closet</h1>
         </header>
         <div id="main">
+          <div className="weather">
+            <h2>test</h2>
+            <h3>test</h3>
+            <p>test</p>
+          </div>
           <Swipeable className="full-height slide-nav" {...config}>
             <ul>
               {/* {this.state.menuArray.map(menuArray => (
@@ -85,16 +76,16 @@ class Main extends Component {
                 </li>
               ))} */}
               <li>
-              <Link to={links[0]}>{names[0]}</Link>
+                <Link to={links[0]}>{names[0]}</Link>
               </li>
               <li>
-              <Link to="#">{names[1]}</Link>
+                <Link to="#">{names[1]}</Link>
               </li>
               <li>
-              <Link to="#">{names[2]}</Link>
+                <Link to="#">{names[2]}</Link>
               </li>
               <li>
-              <Link to="#">{names[3]}</Link>
+                <Link to="#">{names[3]}</Link>
               </li>
             </ul>
           </Swipeable>
